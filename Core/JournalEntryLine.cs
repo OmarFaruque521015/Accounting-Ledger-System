fb50940dd0043a0706dd281442883deb32002fd0
OM@R <faruqueumar512@gmail.com>
2025-06-27 01:18:58 +0600

    Add project files.

---
+++ Core/JournalEntryLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class JournalEntryLine
+    {
+        public int Id {  get; set; }
+        public int JournalEntryId {  get; set; }
+        public JournalEntry JournalEntry { get; set; }
+        public int AccountId {  get; set; }
+        public Account Account { get; set; }
+        public decimal Debit {  get; set; }
+        public decimal Credit {  get; set; }
+
+    }
+}
